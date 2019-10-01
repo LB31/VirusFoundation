e8b8a754efbe63b13010502544c7151e4a43fc35
unknown <leonidb93@gmail.com>
2019-10-01 09:15:48 +0300

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager Instance;
+
+    private void Awake() {
+        if (!Instance) {
+            Destroy(this);
+        } else {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
